b2b69f1351357d993ec6df3e0567fdd01525c9cf
Richard Coleman <richard@microbug.uk>
2018-01-08 17:11:05 +0000

    Cleanup to pass shellcheck

--- init.sh
+++ init.sh
@@ -2,11 +2,10 @@
 
 # Create credentials file
 # Cryptostorm doesn't care what password you use so we use 'foo'
-printf "$CRYPTOSTORM_USERNAME\nfoo" > /config/credentials
-# Remove group and other permissions to remove an openvpn log warning
+printf '%s\nfoo' "$CRYPTOSTORM_USERNAME" > /config/credentials
+
+# Remove group and other permissions (prevents an openvpn log warning)
 chmod go-wrx /config/credentials
-
-
 
 # Check that $PORT is set and it is a number less than 65536
 case $PORT in
@@ -36,19 +35,21 @@ chmod 600 /dev/net/tun
 iptables -A OUTPUT -o tun0 -j ACCEPT # Permit everything from tun0
 iptables -A OUTPUT -o eth0 -p icmp -j ACCEPT # Permit all ICMP from eth0
 iptables -A OUTPUT -d 192.168.0.0/16 -o eth0 -j ACCEPT # Permit all to LAN from eth0
-iptables -A OUTPUT -d 172.16.0.0/12 -o eth0 -j ACCEPT # Permit all to LAN from eth0
-iptables -A OUTPUT -d 10.0.0.0/8 -o eth0 -j ACCEPT # Permit all to LAN from eth0
+iptables -A OUTPUT -d 172.16.0.0/12 -o eth0 -j ACCEPT  # Permit all to LAN from eth0
+iptables -A OUTPUT -d 10.0.0.0/8 -o eth0 -j ACCEPT     # Permit all to LAN from eth0
 
-# Permit elected UDP port if UDP config file is elected, else permit elected TCP port
+# Permit selected UDP port if UDP config file is elected, else permit elected TCP port
+# Shellcheck thinks that "udp.ovpn" is regex and throws an error
+# shellcheck disable=SC2076
 if [[ $CRYPTOSTORM_CONFIG_FILE =~ "udp.ovpn" ]]; then
-    iptables -A OUTPUT -o eth0 -p udp -m udp --dport $PORT -j ACCEPT #permit VPN traffic out of eth0
+    iptables -A OUTPUT -o eth0 -p udp -m udp --dport "$PORT" -j ACCEPT
 else
-    iptables -A OUTPUT -o eth0 -p tcp -m tcp --dport $PORT -j ACCEPT #permit VPN traffic out of eth0
+    iptables -A OUTPUT -o eth0 -p tcp -m tcp --dport "$PORT" -j ACCEPT
 fi
 
-iptables -A OUTPUT -o eth0 -p udp -m udp --dport 53 -j ACCEPT # Permit UDP DNS from eth0 
-iptables -A OUTPUT -o eth0 -p tcp -m tcp --dport 53 -j ACCEPT # Permit TCP DNS from eth0 
+iptables -A OUTPUT -o eth0 -p udp -m udp --dport 53 -j ACCEPT # Permit DNS over UDP from eth0 
+iptables -A OUTPUT -o eth0 -p tcp -m tcp --dport 53 -j ACCEPT # Permit DNS over TCP from eth0 
 iptables -A OUTPUT -o eth0 -j DROP # Drop everything else
 
 # Start openvpn (requires NET_ADMIN)
-openvpn --client --auth-user-pass /config/credentials --config /ovpn-configs/$CRYPTOSTORM_CONFIG_FILE
+openvpn --client --auth-user-pass /config/credentials --config "/ovpn-configs/$CRYPTOSTORM_CONFIG_FILE"
